9974a9f6e5bffb5c8bfe03221a54a46266f25f2e
Guidobeck <alegriarenatoracumin@gmail.com>
2024-10-21 22:34:15 -0400

    move

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -58,7 +58,7 @@ public class Move : MonoBehaviour
         transform.Rotate(0, MouesX, 0);
         PlayerCamera.transform.Rotate(-MouseY, 0, 0);
 
-        transform.Translate(Input.GetAxis("Horizontal") * velocidad * Time.deltaTime, 0, Input.GetAxis("Vertical") * velocidad * Time.deltaTime);
+        transform.Translate(0, Input.GetAxis("Horizontal") * velocidad * Time.deltaTime, Input.GetAxis("Vertical") * velocidad * Time.deltaTime);
 
         
 
